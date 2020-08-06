0b9eb28fc5b8111724a5908e3748d0f97831e80c
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-06 18:17:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    returns array.push ("element")
+    returns array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
